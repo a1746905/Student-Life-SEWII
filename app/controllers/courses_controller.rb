dd20e9b9050e6b1a6e773f4f29c75add49cb464d
Lachie <a1746905@student.adelaide.edu.au>
2019-10-22 21:23:00 +1030

    2 Requirements Functional
    Code is a bit messy but I got the group members drop down list working, as well as the download lecture notes requirement working. Though the courses button on the task bar went funny and I'm not entirely sure why.

---
+++ app/controllers/courses_controller.rb
@@ -0,0 +1,18 @@
+class CoursesController < ApplicationController
+	#Software engineering workshop page
+	def sewii
+		@courseInfo =  "\n \n \n" ;
+		@topic1 = "    Topic 1: Introduction to course";
+		@topic2 = "    Topic 2: ...";
+		@topic3 = "    Topic 3: ...";
+		@topic4 = "    Topic 4: ...";
+		@topic5 = "    Topic 5: ...";
+		@tutorials = "\n \n \n" ;
+	end
+	def topic1
+
+	end
+	def download
+		
+	end
+end
